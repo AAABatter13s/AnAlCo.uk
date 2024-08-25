6d8f6ad032602a792af4d7e18e6e00d9f586807f
Alist <Alist@AnAlCo.uk>
2024-08-25 03:23:45 +0100

    First Commit

---
+++ sw.js
@@ -0,0 +1,45 @@
+// Define the cache name and resources to cache
+var CACHE_NAME = 'my-pwa-cache-1';
+var urlsToCache = [
+  '/',
+  '/index.html'
+];
+
+// Install the service worker and cache resources
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Cache opened');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Serve cached resources when offline
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+// Update the cache with new resources
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return cacheName !== CACHE_NAME;
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
